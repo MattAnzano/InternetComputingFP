ef90de1d197a8a1b84f851b3bfdb68db84ff5acc
[Michael] [Abbondandolo] <71401273+Mike5931@users.noreply.github.com>
2022-04-29 14:52:48 -0400

    fixed navbar

--- signin.php
+++ signin.php
@@ -15,22 +15,20 @@
 <body>
     <nav class="nav nav-pills justify-content navbar navbar-dark bg-secondary">
         <a class="text-sm-center nav-link active navbar-brand" aria-current="page" href="index.php">Welcome to HTMLhelp</a>
-        <div class="dropdown">
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle text-sm-left nav-link active navbar-brand" data-bs-toggle="dropdown" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Sign In/Register</a>
-                <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="signin.php">Sign In</a></li>
-                    <li>
-                        <hr class="dropdown-divider">
-                    </li>
-                    <li><a class="dropdown-item" href="register.php">Register</a></li>
-                    <li>
-                        <hr class="dropdown-divider">
-                    </li>
-                    <li><a class="dropdown-item" href="admin/adminlogin.php">Admin</a></li>
-                </ul>
-            </li>
-        </div>
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle text-sm-left nav-link active navbar-brand" data-bs-toggle="dropdown" role="button" aria-expanded="false">Sign In/Register</a>
+            <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="signin.php">Sign In</a></li>
+                <li>
+                    <hr class="dropdown-divider">
+                </li>
+                <li><a class="dropdown-item" href="register.php">Register</a></li>
+                <li>
+                    <hr class="dropdown-divider">
+                </li>
+                <li><a class="dropdown-item" href="admin/adminlogin.php">Admin</a></li>
+            </ul>
+        </li>
     </nav><br>
     <center>
         <span class="border border-dark" style="display: inline-block;">
@@ -60,6 +58,8 @@
             <button type="submit" class="btn btn-primary">Submit</button>
         </div>
     </form>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
 </body>
 
 </html>
