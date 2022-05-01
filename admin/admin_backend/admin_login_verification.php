8c1894bc4303c9c790d2661553509c1504377c0a
[Michael] [Abbondandolo] <71401273+Mike5931@users.noreply.github.com>
2022-05-01 17:47:10 -0400

    added location if there is no user

--- admin/admin_backend/admin_login_verification.php
+++ admin/admin_backend/admin_login_verification.php
@@ -2,40 +2,39 @@
 require_once '../../db_connection.php';
 
 session_start();
-if(isset($_SESSION['logged_in']) && $_SESSION['logged_in'] == true){
-    $_SESSION['already_li']=true;
+if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] == true) {
+    $_SESSION['already_li'] = true;
     header('Location: ../adminhomepage.php');
 }
 
-$user=strtolower(trim($_POST['Username']));
-$pass=trim($_POST['Password']);
+$user = strtolower(trim($_POST['Username']));
+$pass = trim($_POST['Password']);
 
-if(empty($user) || empty($pass)){
-    $_SESSION['loggin_err']=true;
+if (empty($user) || empty($pass)) {
+    $_SESSION['loggin_err'] = true;
     header('Location: ../adminlogin.php');
-    $db=null;
+    $db = null;
     exit();
 }
-$query=$db->prepare('SELECT Password FROM Admin WHERE Users = :user');
-$query->bindParam(':user',$user);
+$query = $db->prepare('SELECT Password FROM Admin WHERE Users = :user');
+$query->bindParam(':user', $user);
 
 $query->execute();
 $result = $query->fetch();
 
-if(!$result){
-    $_SESSION['no_user']=true;
-
-}
-
-else if($pass == $result['Password']){
-    $_SESSION['logged_in']=true;
-    $_SESSION['user']=$user;
-    $_SESSION['new_log']=true;
+if (!$result) {
+    $_SESSION['no_user'] = true;
+    header('Location: ../adminlogin.php');
+    $db = null;
+    exit();
+} else if ($pass == $result['Password']) {
+    $_SESSION['logged_in'] = true;
+    $_SESSION['user'] = $user;
+    $_SESSION['new_log'] = true;
     header('Location: ../adminhomepage.php');
-}
-else{
-    $_SESSION['wrong_pass']=true;
+} else {
+    $_SESSION['wrong_pass'] = true;
     header('Location: ../adminlogin.php');
 }
-$db=null;
+$db = null;
 exit();
