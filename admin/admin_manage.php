a538f3dce12b3cd3515cd36fddd4d180f4a43cf1
[Michael] [Abbondandolo] <71401273+Mike5931@users.noreply.github.com>
2022-05-02 16:39:52 -0400

    logout

--- admin/admin_manage.php
+++ admin/admin_manage.php
@@ -42,7 +42,10 @@ $result = $query->fetchAll();
 </head>
 
 <body>
-
+    <nav class="nav nav-pills justify-content navbar navbar-dark bg-secondary">
+        <a class="text-sm-center nav-link active navbar-brand" aria-current="page" href="index.php">Welcome to HTMLhelp</a>
+        <a class="nav-link text-sm-center nav-link active navbar-brand" href="../logout.php" role="button" aria-expanded="page">Logout</a>
+    </nav><br>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 
