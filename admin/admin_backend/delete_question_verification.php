caa0ade2ef26bec3cd404545f732553fa4977416
[Michael] [Abbondandolo] <71401273+Mike5931@users.noreply.github.com>
2022-05-04 16:45:15 -0400

    backend deleting questions for admin

--- admin/admin_backend/delete_question_verification.php
+++ admin/admin_backend/delete_question_verification.php
@@ -2,15 +2,24 @@
 require_once '../../db_connection.php';
 session_start();
 
-$question = $_POST['question'];
-$query = $db->prepare('DELETE FROM questions WHERE question = :question');
-$query->bindParam(":question", $question);
+$Question = $_POST['Question'];
+
+if (empty($Question)) {
+    $_SESSION['reg_err'] = true;
+    header('Location: ../manage_questions.php');
+    $db = null;
+    exit();
+}
+
+$query = $db->prepare('DELETE FROM questions WHERE Question = :Question');
+$query->bindParam(":Question", $Question);
 
 if ($query->execute()) {
     $_SESSION['del_success'] = true;
+    header('Location: ../adminhomepage.php');
 } else {
     $_SESSION['del_fail'] = true;
+    header("Location: ../mangae_questions.php");
 }
-header('Location: ../manage_questions.php');
 $db = null;
 exit();
